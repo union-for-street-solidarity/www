89aa6f435d50c91360341e0e473fcd12cc3c40aa
andrewnoyes <andrew@noyes.io>
2019-04-14 12:03:49 -0600

    Publish options

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: 'Gatsby Starter - Dimension V2',
+    author: 'Hunter Chang',
+    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
   },
+  pathPrefix: '/www',
   plugins: [
     'gatsby-plugin-react-helmet',
     {
@@ -19,6 +20,6 @@ module.exports = {
       },
     },
     'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
+    'gatsby-plugin-offline',
   ],
 }
